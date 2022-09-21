5df19fb34944b53748ea6a2d43b872990d05eff0
uma731 <86666586+uma731@users.noreply.github.com>
2022-09-21 14:01:16 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+# app.config.from_object(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def result():
+    var_1 = request.form.get("var_1", type=int, default=0)
+    var_2 = request.form.get("var_2", type=int, default=0)
+    operation = request.form.get("operation")
+    if(operation == 'Addition'):
+        result = var_1 + var_2
+    elif(operation == 'Subtraction'):
+        result = var_1 - var_2
+    elif(operation == 'Multiplication'):
+        result = var_1 * var_2
+    elif(operation == 'Division'):
+    	if(var_1==0 and var_2==0):
+    		result = 0
+    	else:
+        	result = var_1 / var_2
+    else:
+        result = 0
+    entry = result
+    return render_template('index.html', entry=entry)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
